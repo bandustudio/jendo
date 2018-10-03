1a59f822c7e0f388509c778d04ba6bf0287b7a1d
martin <telemagico@gmail.com>
2018-10-03 11:27:13 -0300

    chat ux

--- public/js/routes.js
+++ public/js/routes.js
@@ -34,9 +34,7 @@ function randNick() {
 
 	var first = firsts[Math.floor(Math.random()*firsts.length)];
 	var last = lasts[Math.floor(Math.random()*lasts.length)];
-
 	return `${first} ${last}`;
-	//return [first,last].join(' ');
 }
 
 
@@ -76,19 +74,10 @@ function hexGenerator(len) {
 }
 
 function scrollToBottom () {
-	// Selectors
-	var messages = jQuery('#messages');
-	var newMessage = messages.children('li:last-child')
-	// Heights
-	var clientHeight = messages.prop('clientHeight');
-	var scrollTop = messages.prop('scrollTop');
-	var scrollHeight = messages.prop('scrollHeight');
-	var newMessageHeight = newMessage.innerHeight();
-	var lastMessageHeight = newMessage.prev().innerHeight();
 
-	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
-		messages.scrollTop(scrollHeight);
-	}
+	var objDiv = $(".chat__messages");
+	var h = objDiv.get(0).scrollHeight;
+	objDiv.animate({scrollTop: h});
 }
 
 const Splash = {
@@ -130,11 +119,13 @@ const Chat = {
 			return;
 		}
 
-		var socket = io();
+		
 		var chat = JSON.parse(localStorage.getItem('chat')) || {room:null,name:null};
 		var room = this.$route.params.room;
 		var name = this.$route.params.name || chat.name;
 		var self = this;
+
+		this.socket = io();
 
 		/* verify chat data */
 		if(!name) {
@@ -155,23 +146,22 @@ const Chat = {
 
 		/* socket */
 
-		socket.on('connect', function () {
-
-		  socket.emit('join', store, function (err) {
-		    if (err) {
-		      	alert(err);
-		      	window.location.href = '/';
-		    } else {
-		      	console.log('No error');
+		this.socket.on('connect', function () {
+		  	self.socket.emit('join', store, function (err) {
+			    if (err) {
+			      	alert(err);
+			      	window.location.href = '/';
+			    } else {
+			      	console.log('No error');
 		    	}
 		  	});
 		});
 
-		socket.on('disconnect', function () {
+		this.socket.on('disconnect', function () {
 		  	console.log('Disconnected from server');
 		});
 
-		socket.on('updateUserList', function (users) {
+		this.socket.on('updateUserList', function (users) {
 			var ul = jQuery('<ul></ul>');
 
 			users.forEach(function (user,i) {
@@ -187,7 +177,7 @@ const Chat = {
 			jQuery('#users').html(ul);
 		});
 
-		socket.on('newMessage', function (message) {
+		this.socket.on('newMessage', function (message) {
 			var formattedTime = moment(message.createdAt).format('HH:mm');
 			var template = jQuery('#message-template').html();
 			var html = Mustache.render(template, {
@@ -201,7 +191,7 @@ const Chat = {
 			scrollToBottom();
 		});
 
-		socket.on('newLocationMessage', function (message) {
+		this.socket.on('newLocationMessage', function (message) {
 
 			var connected = Object.keys(self.markers).length;
 
@@ -261,7 +251,7 @@ const Chat = {
 	  	} else {
 
 		  	navigator.geolocation.watchPosition(function(position){
-			    socket.emit('createLocationMessage', {
+			    self.socket.emit('createLocationMessage', {
 			      	latitude: position.coords.latitude,
 			      	longitude: position.coords.longitude
 			    });
@@ -273,42 +263,25 @@ const Chat = {
 	        	maximumAge: 5000 // 5 sec.
 	      	});
 		}
-
-		/* click events */
-
-		jQuery('#message-form').on('submit', function (e) {
-		  e.preventDefault();
-
-		  var messageTextbox = jQuery('[name=message]');
-
-		  socket.emit('createMessage', {
-		    text: messageTextbox.val()
-		  }, function () {
-		    messageTextbox.val('')
-		  });
-		});
-
-		var locationButton = jQuery('#send-location');
-		locationButton.on('click', function () {
-		  	if (!navigator.geolocation) {
-		    	return alert('Geolocation not supported by your browser.');
-		  	}
-
-		  	locationButton.attr('disabled', 'disabled').text('Sending location...');
-
-		  	navigator.geolocation.getCurrentPosition(function (position) {
-		    	locationButton.removeAttr('disabled').text('Send location');
-			    socket.emit('createLocationMessage', {
-			      	latitude: position.coords.latitude,
-			      	longitude: position.coords.longitude
-			    });
-		  	}, function () {
-		    	locationButton.removeAttr('disabled').text('Send location');
-		    	alert('Unable to fetch location.');
-		  	});
-		});		
 	},
 	methods: {
+		sendMessage:function({type,target}){
+			var messageTextbox = $('input[name="message"]');
+			this.socket.emit('createMessage', {
+				text: messageTextbox.val()
+			}, function () {
+				messageTextbox.val('')
+			});
+		},
+		toggleChat:function({type,target}){
+			var span = $(target).find('.fa');
+			if(span.hasClass('fa-toggle-on')){
+				span.removeClass('fa-toggle-on').addClass('fa-toggle-off');
+			} else {
+				span.removeClass('fa-toggle-off').addClass('fa-toggle-on');
+			}
+			$('.chat__messages').toggle();
+		},
 		initLayers:function(){
 			var styleList = document.getElementById('styles');
 			styleList.onchange = this.switchLayer
@@ -333,7 +306,7 @@ const Chat = {
 	},
 	data: function() {
 		return{
-			connected:0,
+			socket:null,
 		  	map:null,
 		  	name:null,
 		  	room:null,
